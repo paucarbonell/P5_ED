0834f4297d29c3d770c46311442330c8a4d459b4
paucarbonell <paucarbonellpasero@gmail.com>
2023-10-01 13:33:05 +0200

    Commit a repositorio en NetBeans

---
+++ OneDrive/Documentos/NetBeansProjects/saludo/src/main/java/com/mycompany/saludo/Saludo.java
@@ -0,0 +1,12 @@
+package com.mycompany.saludo;
+
+/**
+ *
+ * @author Pau
+ */
+public class Saludo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Pau!");
+    }
+}
